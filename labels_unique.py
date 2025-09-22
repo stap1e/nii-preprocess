540fd1302afd72e0aeb63c6a1da094ea1475d599
stap1e <1602564548@qq.com>
2025-09-22 18:51:36 +0800

    label-unique

--- labels_unique.py
+++ labels_unique.py
@@ -2,14 +2,15 @@ import h5py, os, torch
 import SimpleITK as sitk
 
 def main():
-    label_path = "E:/FLARE-Lab/FLARE25/test_labels"
+    # label_path = "E:/FLARE-Lab/FLARE25/test_labels" # flare
+    label_path = "E:/AbdomenCT-1K/Mask" # AK
     labelnii_names = sorted(os.listdir(label_path)) 
 
     for i, label_name in enumerate(labelnii_names):
         sitk_label = sitk.ReadImage(os.path.join(label_path, label_name))
         label = sitk.GetArrayFromImage(sitk_label)
-        label = torch.from_numpy(label).long().cuda()
-        print(f"{(i+1):02d}-th, file nme: {label_name},  test label unique: {label.unique().cpu().tolist()}")
+        label = torch.from_numpy(label).long()
+        print(f"{(i+1):04d}-th, file nme: {label_name},  test label unique: {label.unique().cpu().tolist()}")
 
 if __name__ == '__main__':
     main()
