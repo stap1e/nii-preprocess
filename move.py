6093f97100fcd107e5fa8dd4eb6a259508906b67
stap1e <1602564548@qq.com>
2025-09-25 15:27:21 +0800

    move

--- move.py
+++ move.py
@@ -2,27 +2,26 @@ import os, shutil
 
 
 def main():
-    h5_path = "E:/AbdomenCT-1K/test1/labeled_h5" # AK
-    unlabeled_names_file = "E:/AbdomenCT-1K/test1/notuse.txt" # AK
-    unlabeled_path = "E:/AbdomenCT-1K/test1/labeled_h5_notuse" # AK
+    unlabeled_path = "E:/AbdomenCT-1K/test2/labeled_notuse_h5" # AK
     os.makedirs(unlabeled_path, exist_ok=True)
+    unlabeled_names_file = "E:/AbdomenCT-1K/test2/notuse.txt" # AK
+    labeled_path = "E:/AbdomenCT-1K/test2/labeled_h5" # AK
     index = 0
-    file_names = sorted(os.listdir(h5_path))
     with open(unlabeled_names_file, 'r') as f:
-        names_file = f.readlines()
-    names_file = [name.strip() for name in names_file]
-    for name in file_names:
-        if name.split('.')[-1] != 'h5':
-            continue
-        if name.split('.h5')[0] in names_file:
-            index += 1
-            destination_path = os.path.join(unlabeled_path, name)
-            source_path = os.path.join(h5_path, name)
-            try:
-                shutil.move(source_path, destination_path)
-                print(f"成功移动文件：'{name}'")
-            except Exception as e:
-                print(f"移动文件 '{name}' 时出错：{e}")
+        file_names = f.readlines()
+    file_names = [item.strip() for item in file_names]
+    linu_f_names = sorted(os.listdir(labeled_path)) 
+    linu_f_names = [name.split('.h5')[0] for name in linu_f_names]
+    names_file = [name for name in linu_f_names if name in file_names]
+    for name in names_file:
+        index += 1
+        destination_path = os.path.join(unlabeled_path, name + '.h5')
+        source_path = os.path.join(labeled_path, name + '.h5')
+        try:
+            shutil.move(source_path, destination_path)
+            print(f"成功移动文件：'{name + '.h5'}'")
+        except Exception as e:
+            print(f"移动文件 '{name + '.h5'}' 时出错：{e}")
     print(f"Total moved files num: {index}")
 
 
